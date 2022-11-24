a5c30c1438a843cf4d89b240a7c6440507f0959c
ByAhmetM <118487365+ByAhmetM@users.noreply.github.com>
2022-11-24 23:25:24 +0300

    24.11 23.25

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,7 +4,7 @@ export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <h3>{this.props.info.title}</h3>
+        <h3>{this.props.info.title}-<h4>{this.props.currentCategory}</h4></h3>
       </div>
     );
   }
